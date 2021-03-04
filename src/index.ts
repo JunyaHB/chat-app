cea9535f3046419a708bc6746b873325fbf375e9
JunyaHB <a1933jh@aiit.ac.jp>
2021-03-04 17:40:35 +0900

    Defining Queries and Mutations

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import { buildSchema } from "type-graphql";
 import cors from "cors";
 import dotenv from "dotenv";
+import { ChatResolver } from "./resolvers/chat";
 
 dotenv.config();
 
